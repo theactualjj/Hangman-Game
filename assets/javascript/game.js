3a20667dae6d8b1868deee844f8f008bb10c1807
Jeremy Johnson <jeremyjohnson@Jeremys-MacBook-Air.local>
2017-08-11 12:38:18 -0500

    Rev 4 Updates Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-prompt("Press any key to begin!")
+prompt("Press any key to begin!");
 
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
